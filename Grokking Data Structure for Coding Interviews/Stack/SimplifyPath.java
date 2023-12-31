705b79657a6d2f2d97752d44fd86a70524678ea1
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2023-12-30 22:53:40 -0800

    Add SimplifyPath class and test cases

---
+++ Grokking Data Structure for Coding Interviews/Stack/SimplifyPath.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class SimplifyPath {
+    public static String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        for (String s : path.split("/")) {
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!s.isEmpty() && !s.equals(".")) {
+                stack.push(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : stack) {
+            sb.append("/");
+            sb.append(s);
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        System.out.println(simplifyPath("/a//b////c/d//././/..")); // Expected output: "/a/b/c"
+        System.out.println(simplifyPath("/../")); // Expected output: "/"
+        System.out.println(simplifyPath("/home//foo/")); // Expected output: "/home/foo"
+    }
+}
