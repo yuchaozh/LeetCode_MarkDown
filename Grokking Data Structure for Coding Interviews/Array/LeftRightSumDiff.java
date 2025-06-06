ed72523793f0952b26a0d109ba40f8ebaccdcd41
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2025-06-06 11:58:06 -0700

    feat: Add findDifferenceArrayV2 method for calculating the difference between left and right sums

--- Grokking Data Structure for Coding Interviews/Array/LeftRightSumDiff.java
+++ Grokking Data Structure for Coding Interviews/Array/LeftRightSumDiff.java
@@ -25,6 +25,26 @@ class LeftRightSumDiff {
         return differenceArray;
     }
 
+    public int[] findDifferenceArrayV2(int[] nums) {
+        int length = nums.length;
+        int rightSum = 0, leftSum = 0;
+        int[] differenceArray = new int[length];
+
+        // Calculate the total sum of the array
+        for (int i = 0; i < length; i++) {
+            rightSum += nums[i];
+        }
+
+        // Calculate the difference between left and right sums for each position
+        for (int i = 0; i < length; i++) {
+            rightSum -= nums[i];
+            differenceArray[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+
+        return differenceArray;
+    }
+
     public static void main(String[] args) {
         LeftRightSumDiff solution = new LeftRightSumDiff();
         int[] example1 = { 2, 5, 1, 6 };
@@ -32,11 +52,11 @@ class LeftRightSumDiff {
         int[] example3 = { 1, 2, 3, 4, 5 };
 
         // Output should be: [12, 5, 1, 8]
-        printArray(solution.findDifferenceArray(example1));
+        printArray(solution.findDifferenceArrayV2(example1));
         // Output should be: [9, 5, 0, 6, 10]
-        printArray(solution.findDifferenceArray(example2));
+        printArray(solution.findDifferenceArrayV2(example2));
         // Output should be: [14, 11, 6, 1, 10]
-        printArray(solution.findDifferenceArray(example3));
+        printArray(solution.findDifferenceArrayV2(example3));
     }
 
     // Helper method to print array elements
