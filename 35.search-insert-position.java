472ac09c38973cae3efb91e252f82d21ac94992b
Zhou Yuchao <yuchaozh1991@gmail.com>
2026-04-01 23:33:31 -0700

    feat: Implement searchInsert method for finding the insert position in a sorted array

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -56,7 +56,25 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = ++mid;
+            } else {
+                right = --mid;
+            }
+        }
+
+        return left;
     }
 }
 // @lc code=end
