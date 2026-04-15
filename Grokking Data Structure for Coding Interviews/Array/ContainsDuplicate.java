dafe18aefaf96676d39b54f1d9c95db7782b3ff7
Zhou Yuchao <yuchaozh1991@gmail.com>
2026-04-14 22:40:24 -0700

    feat: Add methods to check for duplicates using two loops, sorting with one loop, and adjacent comparison

--- Grokking Data Structure for Coding Interviews/Array/ContainsDuplicate.java
+++ Grokking Data Structure for Coding Interviews/Array/ContainsDuplicate.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,6 +11,54 @@ public class ContainsDuplicate {
         return true;
     }
     return false; // Return false if no duplicates found
+  }
+
+  // two loops
+  public boolean containsDuplicateV2(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return false;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  // sort and one loop check
+  public boolean containsDuplicateV3(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return false;
+    }
+
+    Arrays.sort(nums);
+
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i - 1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // check the first element with the next element so that we dont need to check
+  // nums == null || nums.length < 2
+  public boolean containsDuplicateV4(int[] nums) {
+    Arrays.sort(nums); // sort the array
+    // use a loop to compare each element with its next element
+    for (int i = 0; i < nums.length - 1; i++) {
+      // if any two elements are the same, return true
+      if (nums[i] == nums[i + 1]) {
+        return true;
+      }
+    }
+    return false; // if no duplicates are found, return false
   }
 
   public static void main(String[] args) {
